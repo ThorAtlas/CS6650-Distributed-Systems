793a330726b9fb42cc6d260b0d7a6b90c0782c1d
ThorAtlas <jarredg2018@gmail.com>
2021-11-14 17:40:47 -0500

    Attempted Edits to Docker file stuff and deploy

--- Repos/DisSystems/ScalableProject3/src/deploy.sh
+++ Repos/DisSystems/ScalableProject3/src/deploy.sh
@@ -1,22 +1,27 @@
-PROJECT_NETWORK='project2-network'
-SERVER_IMAGE='project2-hashserverimpl-image'
+PROJECT_NETWORK='project3-network'
+SERVER_IMAGE='project3-controllerserver-image'
 
+CONTROLLER_CLIENT_CONTAINER='my-controllerserver'
 HASHSERVER_CONTAINER='my-hashserver'
-CLIENT_IMAGE='project2-hashclient-image'
+CLIENT_IMAGE='project3-hashclient-image'
 CLIENT_CONTAINER='my-hashclient'
 
 # clean up existing resources, if any
 echo "----------Cleaning up existing resources----------"
+docker container stop $CONTROLLER_CLIENT_CONTAINER 2> /dev/null && docker container rm $CONTROLLER_CLIENT_CONTAINER 2> /dev/null
 docker container stop $HASHSERVER_CONTAINER 2> /dev/null && docker container rm $HASHSERVER_CONTAINER 2> /dev/null
-#docker container stop $UDPSERVER_CONTAINER 2> /dev/null && docker container rm $UDPSERVER_CONTAINER 2> /dev/null
 docker container stop $CLIENT_CONTAINER 2> /dev/null && docker container rm $CLIENT_CONTAINER 2> /dev/null
 docker network rm $PROJECT_NETWORK 2> /dev/null
 
-
+CONTROLLERIMPORT=1111
 POSTIMPORT=1099
 if [ $# -eq 1 ]
 then
-  POSTIMPORT="$1"
+  CONTROLLERIMPORT="$1"
+elif [ $# -eq 2 ]
+then
+  POSTIMPORT="$2"
+  CONTROLLERIMPORT="$1"
 fi
 
 
@@ -33,5 +38,11 @@ docker build -t $SERVER_IMAGE --target HashServerImpl-build .
 # run the image and open the required ports
 echo "----------Running sever app----------"
 docker run --name \
-$HASHSERVER_CONTAINER --network $PROJECT_NETWORK $SERVER_IMAGE java Server/HashServerImpl "$POSTIMPORT"
+$CONTROLLER_CLIENT_CONTAINER --network $PROJECT_NETWORK $SERVER_IMAGE java Server/ControllerImpl \
+"$CONTROLLERIMPORT"
+docker run --name \
+$HASHSERVER_CONTAINER --network $PROJECT_NETWORK $SERVER_IMAGE java Server/HashServerImpl \
+"$POSTIMPORT" "$CONTROLLERIMPORT"
+
+
 #docker run -d -p 1234:1234/udp --name $UDPSERVER_CONTAINER --network $PROJECT_NETWORK $UDPSERVER_IMAGE
